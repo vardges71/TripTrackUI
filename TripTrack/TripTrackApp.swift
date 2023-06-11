669bbf23f6464cc6f3ecb9bbfde177499fc9f1b2
vardges71 <vardges71@gmail.com>
2023-06-11 01:00:22 -0400

    Initial commit

---
+++ TripTrack/TripTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripTrackApp.swift
+//  TripTrack
+//
+//  Created by Vardges Gasparyan on 2023-06-11.
+//
+
+import SwiftUI
+
+@main
+struct TripTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
